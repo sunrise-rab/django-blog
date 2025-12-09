c120e70e5d78ab5e24d876d4926a0cf25eeac76e
sunrise-rab <rabia.tsdi@gmail.com>
2025-12-09 22:41:52 +0000

    Add SECRET_KEY to env.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -90,6 +90,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
