cf42bf070c33a910730c71e559b0e5412d5fb118
sunrise-rab <rabia.tsdi@gmail.com>
2025-12-14 16:29:34 +0000

    modify the standard admin dashboard using both Django methods and integrating the Summernote package

--- codestar/settings.py
+++ codestar/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
--- codestar/urls.py
+++ codestar/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("blog/", include("blog.urls"), name="blog-urls"),
+    path("", include("blog.urls"), name="blog-urls"),
+    path('summernote/', include('django_summernote.urls')),
 
 ]
