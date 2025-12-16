b7843d4a1b713b72c28b777cadfa679d1918714f
sunrise-rab <rabia.tsdi@gmail.com>
2025-12-16 23:02:07 +0000

    create template folder and about/ urls. also update the codestar/urls

--- about/views.py
+++ about/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.views import generic
 from .models import About
 
 # Create your views here.
-class AboutList(generic.ListView):
-    queryset= About.objects.all()
-    
+
+def about_me(request):
+    """
+    Renders the About page
+    """
+    about = About.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
--- codestar/settings.py
+++ codestar/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'about',
     'django_summernote',
 ]
 
