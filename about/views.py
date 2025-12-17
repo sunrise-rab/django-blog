42aff3554d2037161047d36870f15466b9e68c92
sunrise-rab <rabia.tsdi@gmail.com>
2025-12-17 13:05:57 +0000

    Update the view function

--- about/views.py
+++ about/views.py
@@ -1,10 +1,16 @@
-from django.db import models
+from django.shortcuts import render
+from .models import About
 
+# Create your views here.
 
-class About(models.Model):
-    title = models.CharField(max_length=200)
-    updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
+def about_me(request):
+    """
+    Renders the About page
+    """
+    about = About.objects.all().order_by('-updated_on').first()
 
-    def __str__(self):
-        return self.title
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
