ab784618abe8faccd2244b1260486e3d0f77b0f6
sunrise-rab <rabia.tsdi@gmail.com>
2025-12-20 21:29:51 +0000

    Adding a CloudinaryImage field to the About model

--- about/models.py
+++ about/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 class About(models.Model):
+    profile_image = CloudinaryField('image',default='placeholder')
     title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
