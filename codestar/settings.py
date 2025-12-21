6f12ac7523c1e2e25d218ab7288c01e3ac1ae1d8
sunrise-rab <rabia.tsdi@gmail.com>
2025-12-21 12:18:02 +0000

    Turn debug to true

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-x2mubr$70huvos5%tm_f*l7gs179)39@s9%z6%rhva*u5l3)zp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
